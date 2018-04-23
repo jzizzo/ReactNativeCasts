38b868186557a0e411708561777caa8607580e10
Joseph Zizzo <Joseph.Zizzo@logmein.com>
2018-04-22 18:02:46 -0700

    Add firebase authentication
    Add input and spinner functionality
    Add login functionality
    Add logout functionality

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+import firebaseConfig from './FirebaseConfig';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
       <Header headerText="Authentication" />
-        <Text>Hello world!</Text>
+        {this.renderContent()}
       </View>
     );
   }
