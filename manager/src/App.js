198fd67c19e19f6b709d097b12aabbf8487d225e
Joseph Zizzo <Joseph.Zizzo@logmein.com>
2018-04-29 18:49:26 -0700

    Add initial Manager app seed project
    Add react, redux, react-redux
    Add firebase configuration and hide API key
    Add eslint

---
+++ manager/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import firebaseConfig from './FirebaseConfig';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
